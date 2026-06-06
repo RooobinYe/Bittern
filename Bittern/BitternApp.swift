7a5d3ffa3a9e1c5fa1d16b4c278ae8259f7b474c
Ye Zhenrong <3471626272@qq.com>
2026-06-06 12:10:29 +0800

    Initial Commit

---
+++ Bittern/BitternApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitternApp.swift
+//  Bittern
+//
+//  Created by 叶桢荣 on 2026/6/6.
+//
+
+import SwiftUI
+
+@main
+struct BitternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bittern/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bittern
+//
+//  Created by 叶桢荣 on 2026/6/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
